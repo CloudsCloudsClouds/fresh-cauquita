405d7774839e534089b6462738016e312ebb35a0
CloudsCloudsClouds <56483061+CloudsCloudsClouds@users.noreply.github.com>
2025-11-11 19:17:02 -0400

    Better dev

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,6 +2,7 @@ import { useSignal } from "@preact/signals";
 import { Head } from "fresh/runtime";
 import { define } from "../utils.ts";
 import Counter from "../islands/Counter.tsx";
+import TheCoolerButton from "../islands/TheCoolerButton.tsx";
 
 
 
@@ -12,16 +13,13 @@ export default define.page(function Home(ctx) {
   console.log("Shared value " + ctx.state.shared);
 
   return (
-    <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
+    <div class="px-4 py-8 mx-auto min-h-screen">
       <Head>
         <title>Cauquita Racer</title>
       </Head>
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <h1 class="text-4xl font-bold mb-4 text-blue-950"> Bienvenido Cauquita Racer</h1>
-        <a href="/inicio" class="mt-8 text-yellow-500" data-f-partial>
-          Empezamos
-          <img src="" alt="" />
-        </a>
+        <TheCoolerButton coolerLink="/inicio" />
 
       </div>
     </div>
