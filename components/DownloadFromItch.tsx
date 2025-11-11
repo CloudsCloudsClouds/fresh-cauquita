fb262a46144daeaae25e66a30bb6ae7f95edd848
CloudsCloudsClouds <56483061+CloudsCloudsClouds@users.noreply.github.com>
2025-11-11 19:27:21 -0400

    component, implemented

---
+++ components/DownloadFromItch.tsx
@@ -0,0 +1,7 @@
+export default function DownloadFromItch() {
+    return (
+        <iframe frameborder="0" src="https://itch.io/embed/4032932" width="552" height="167">
+            <a href="https://triangled-pineapple.itch.io/ultimate-cauquita-racer">Ultimate Cauquita Racer by Triangled Pineapple</a>
+        </iframe>
+    );
+}
