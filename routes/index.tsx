d2f413a67299f94a76c090a03262a94e4e4123d2
Fabricio44S <fabriciosebastian2000@gmail.com>
2025-11-25 19:16:17 -0400

    mejorando titulos

--- routes/index.tsx
+++ routes/index.tsx
@@ -16,9 +16,17 @@ export default define.page(function Home(ctx) {
     <div class="px-4 py-8 mx-auto min-h-screen">
       <Head>
         <title>Cauquita Racer</title>
+         <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+            rel="stylesheet"
+          />
+        <link rel="stylesheet" href="/styles.css" />
       </Head>
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <h1 class="text-4xl font-bold mb-4 text-blue-950"> Bienvenido Cauquita Racer</h1>
+
+        <h1 class="pixel-inicio text-3xl md:text-5xl"> Bienvenido Cauquita Racer</h1>
         <TheCoolerButton coolerLink="/inicio" />
 
       </div>
